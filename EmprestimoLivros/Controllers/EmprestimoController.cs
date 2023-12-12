8d54dff64ff7a6f978a9092f26967591c4218309
Gabriel Almeida <41032795+gabrielmagal@users.noreply.github.com>
2023-12-12 20:36:53 -0300

    emprestimo de livros

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,112 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _context;
+
+        public EmprestimoController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+            return View(emprestimos);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimosModel)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Emprestimos.Add(emprestimosModel);
+                _context.SaveChanges();
+                TempData["MensagemSucesso"] = "Cadastro realizado com sucesso!";
+                return RedirectToAction("Index");
+            }
+            TempData["MensagemError"] = "Cadastro não realizado!";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                TempData["MensagemError"] = "Edição não realizada!";
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if(emprestimo == null)
+            {
+                TempData["MensagemError"] = "Edição não realizada!";
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Emprestimos.Update(emprestimo);
+                _context.SaveChanges();
+                TempData["MensagemSucesso"] = "Edição realizada com sucesso!";
+                return RedirectToAction("Index");
+            }
+            TempData["MensagemError"] = "Edição não realizada!";
+            return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["MensagemError"] = "Remoção não realizada!";
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null)
+            {
+                TempData["MensagemError"] = "Remoção não realizada!";
+                return NotFound();
+            }
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                TempData["MensagemError"] = "Remoção não realizada!";
+                return NotFound();
+            }
+
+            _context.Emprestimos.Remove(emprestimo);
+            _context.SaveChanges();
+            TempData["MensagemSucesso"] = "Remoção realizada com sucesso!";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $('#Emprestimos').DataTable({
+            language: {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ registro de _END_ em um total de _TOTAL_ entradas",
+            "infoEmpty": "Exibindo 0 de 0 em um total de 0 entradas",
+            "infoFiltered": "(Filtro aplicado em um total de _MAX_ entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Carregando...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "Sem resultados encontrados para exibir.",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
+    });
+
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        })
+    }, 5000);
+});
